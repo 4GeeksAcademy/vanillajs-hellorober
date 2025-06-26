7b4d8a830accd275c4b125207266996c47839450
s0ldyer <sol_dier5@hotmail.es>
2025-06-26 15:33:30 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+function getRandonNumber(a,b){
+return Math.floor(Math.random()*(b-a)+a);
+}
+
+function randomnumber(array){
+  let maximo=array.length-1;
+  let minimo=0;
+  let random=getRandonNumber(minimo,maximo);
+  return array[random];
+}
+
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+ document.querySelector("#excuse").innerHTML= randomnumber(who)+" "+randomnumber(action)+" "+randomnumber(what)+" "+randomnumber(when)}; 
+
+
+
+
